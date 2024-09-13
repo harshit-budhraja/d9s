02d48a017e08dec544311369b42e6157e69ddc8f
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2024-09-13 06:44:37 +0530

    feat: working container display

--- source/app.tsx
+++ source/app.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Box, Key, Newline, Text, useInput } from 'ink';
 import Docker from 'dockerode';
 import terminalSize from 'terminal-size';
+import BigText from 'ink-big-text';
+import Gradient from 'ink-gradient';
 
 const dkrVersion = '0.0.1';
 
@@ -23,12 +25,15 @@ const colorMap: {
 	dkr_blue: '#0f63cc',
 	dkr_gray: '#b6b1b7',
 	dkr_lightblue: '#84cbfa',
+	dkr_highlightblue: '#28abee',
 };
 
 const docker = new Docker();
 
 export default function App() {
 	const [dockerInfo, setDockerInfo] = useState<any>(null);
+	const [containers, setContainers] = useState<Docker.ContainerInfo[]>([]);
+	const [selectedRow, setSelectedRow] = useState<number>(6);
 
 	useEffect(() => {
 		console.clear();
@@ -36,7 +41,9 @@ export default function App() {
 		const fetchData = async () => {
 			try {
 				const dockerInfo = await docker.info();
+				const containerInfo = await docker.listContainers();
 				setDockerInfo(dockerInfo);
+				setContainers(containerInfo);
 			} catch (err: any) {
 				console.error(err);
 			}
@@ -50,32 +57,59 @@ export default function App() {
 			console.clear();
 			process.exit(0);
 		}
+
+		if (key.downArrow || key.upArrow) {
+			if (key.downArrow) {
+				setSelectedRow(selectedRow + 1);
+			} else if (key.upArrow) {
+				setSelectedRow(selectedRow - 1);
+			}
+		}
 	});
+
+	if (!dockerInfo) {
+		return (
+			<Box>
+				<Text>Loading...</Text>
+			</Box>
+		);
+	}
+
+	const selectedRowProps = {
+		color: colorMap['dkr_lightblue'],
+		bold: true,
+		underline: true,
+	};
+
+	const unselectedRowProps = {
+		color: colorMap['dkr_lightblue'],
+		dimColor: true,
+	};
 
 	return (
 		<Box flexDirection="column" height={terminalSize().rows - 1}>
-			{dockerInfo && (
+			<Box flexDirection="row" justifyContent="space-between" marginBottom={-1}>
 				<Box flexDirection="column" marginLeft={1}>
 					<Text>
 						<Text color={colorMap['dkr_orange']}>Context: </Text>
 						{
 							dockerInfo.OperatingSystem ?
-							<Text bold>{dockerInfo.OperatingSystem}</Text> :
-							<Text bold color={colorMap['dkr_brown']}>n/a</Text>
+								<Text bold>{dockerInfo.OperatingSystem}</Text> :
+								<Text bold color={colorMap['dkr_brown']}>n/a</Text>
 						}
 						<Newline />
 						<Text color={colorMap['dkr_orange']}>User: </Text>
 						{
 							dockerInfo.Name ?
-							<Text bold>{dockerInfo.Name}</Text> :
-							<Text bold color={colorMap['dkr_brown']}>n/a</Text>
+								<Text bold>{dockerInfo.Name}</Text> :
+								<Text bold color={colorMap['dkr_brown']}>n/a</Text>
 						}
 						<Newline />
 						<Text color={colorMap['dkr_orange']}>Platform: </Text>
 						{
 							dockerInfo.OSType && dockerInfo.Architecture ?
-							<Text bold>{dockerInfo.OSType} ({dockerInfo.Architecture})</Text> :
-							<Text bold color={colorMap['dkr_brown']}>n/a</Text>
+								<Text bold>{dockerInfo.OSType} ({dockerInfo.Architecture})</Text> :
+								<Text bold color={colorMap['dkr_brown']}>n/a</Text>
 						}
 						<Newline />
 						<Text color={colorMap['dkr_orange']}>Dkr Rev: </Text>
@@ -84,30 +118,99 @@ export default function App() {
 						<Text color={colorMap['dkr_orange']}>Docker Rev: </Text>
 						{
 							dockerInfo.ServerVersion ?
-							<Text bold>v{dockerInfo.ServerVersion}</Text> :
-							<Text bold color={colorMap['dkr_brown']}>n/a</Text>
+								<Text bold>v{dockerInfo.ServerVersion}</Text> :
+								<Text bold color={colorMap['dkr_brown']}>n/a</Text>
 						}
 						<Newline />
 						<Text color={colorMap['dkr_orange']}>CPUs: </Text>
 						{
 							dockerInfo.NCPU ?
-							<Text bold>{dockerInfo.NCPU}</Text> :
-							<Text bold color={colorMap['dkr_brown']}>n/a</Text>
+								<Text bold>{dockerInfo.NCPU}</Text> :
+								<Text bold color={colorMap['dkr_brown']}>n/a</Text>
 						}
 						<Newline />
 						<Text color={colorMap['dkr_orange']}>MEM: </Text>
 						{
 							dockerInfo.MemTotal ?
-							<Text bold>{Math.ceil(dockerInfo.MemTotal / (1024 * 1024 * 1024)).toFixed(2)} GB</Text> :
-							<Text bold color={colorMap['dkr_brown']}>n/a</Text>
+								<Text bold>{Math.ceil(dockerInfo.MemTotal / (1024 * 1024 * 1024)).toFixed(2)} GB</Text> :
+								<Text bold color={colorMap['dkr_brown']}>n/a</Text>
 						}
 					</Text>
 				</Box>
-			)}
+
+				<Box flexDirection="column" marginRight={140}>
+					<Text>
+						<Text color={colorMap['dkr_blue']} bold>{"<ctrl+c>: "}</Text>
+						<Text color={colorMap['dkr_gray']}>quit</Text>
+						<Newline />
+						<Text color={colorMap['dkr_blue']} bold>{"<up-arrow>: "}</Text>
+						<Text color={colorMap['dkr_gray']}>move up</Text>
+						<Newline />
+						<Text color={colorMap['dkr_blue']} bold>{"<down-arrow>: "}</Text>
+						<Text color={colorMap['dkr_gray']}>move down</Text>
+					</Text>
+				</Box>
+
+				<Box marginRight={3}>
+					<Gradient name="rainbow">
+						<BigText text='dkr' font='simple' letterSpacing={0} />
+					</Gradient>
+				</Box>
+			</Box>
 
 			<Box flexGrow={1} borderStyle="single" borderColor={colorMap['dkr_lightblue']}>
-				<Box>
-					<Text color={colorMap['dkr_lightblue']}>Containers</Text>
+				<Box flexDirection="column" gap={0}>
+					<Box justifyContent="flex-start" width={terminalSize().columns - 1} marginLeft={1}>
+						<Box>
+							<Text bold color="cyan">Containers[{<Text color="yellow">{containers.filter(container => !container.Labels.hasOwnProperty('io.kubernetes.pod.uid')).length}</Text>}]</Text>
+						</Box>
+					</Box>
+
+					<Box flexDirection="row" justifyContent="space-between" width={terminalSize().columns - 10} marginLeft={1} marginTop={1}>
+						<Box width={30}>
+							<Text bold>NAME</Text>
+						</Box>
+						<Box width={20}>
+							<Text bold>IMAGE</Text>
+						</Box>
+						<Box width={20}>
+							<Text bold>STATUS</Text>
+						</Box>
+						<Box width={20}>
+							<Text bold>PORTS</Text>
+						</Box>
+						<Box width={20}>
+							<Text bold>AGE</Text>
+						</Box>
+					</Box>
+
+					{
+						containers.map((container, index) => {
+							if (!container.Labels.hasOwnProperty('io.kubernetes.pod.uid')) {
+								return (
+									<Box key={index} flexDirection="row" justifyContent="space-between" width={terminalSize().columns - 10} marginLeft={1}>
+										<Box width={30}>
+											<Text {...(index === selectedRow ? selectedRowProps : unselectedRowProps)}>{container.Names[0]!.replace('/', '')}</Text>
+										</Box>
+										<Box width={20}>
+											<Text {...(index === selectedRow ? selectedRowProps : unselectedRowProps)}>{container.Image}</Text>
+										</Box>
+										<Box width={20}>
+											<Text {...(index === selectedRow ? selectedRowProps : unselectedRowProps)}>{container.State}</Text>
+										</Box>
+										<Box width={20}>
+											<Text {...(index === selectedRow ? selectedRowProps : unselectedRowProps)}>{container.Ports.map(port => port.IP === '0.0.0.0' ? port.PublicPort : null).filter(port => port).join(",")}</Text>
+										</Box>
+										<Box width={20}>
+											<Text {...(index === selectedRow ? selectedRowProps : unselectedRowProps)}>{container.Status}</Text>
+										</Box>
+									</Box>
+								);
+							}
+
+							return null;
+						})
+					}
 				</Box>
 			</Box>
 
